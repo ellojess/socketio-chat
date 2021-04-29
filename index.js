52b1054842fba03af24f3b602257f9c361a36944
Jess <jtrinh619@gmail.com>
2021-04-28 23:40:15 -0700

    bug fix, messages now emit to all users

--- index.js
+++ index.js
@@ -10,18 +10,11 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-});
-
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-      io.emit('chat message', msg);
-    });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
+});
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
