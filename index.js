9bd84e0c6b6a7253fb695fdd5760f3d554b96791
Jess <jtrinh619@gmail.com>
2021-05-11 01:08:47 -0700

    broadcast connect/disconnect and add support for nicknames

--- index.js
+++ index.js
@@ -9,12 +9,26 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', (socket) => {
+  
+  let current_user
+
+  socket.on('name', name => {
+    current_user = name
+    io.emit('broadcast', `${current_user} has entered`)
+
+    socket.on('disconnect', () => {
+      io.emit('broadcast', `${current_user} has left`)
+    })
+  })
+
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-});
+    // inform others of message sent 
+    io.emit('chat message', `[ ${current_user} ] : ${msg}`)
+  })
+})
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
